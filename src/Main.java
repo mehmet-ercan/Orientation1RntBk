5eb622688b4c432fc5fa2433516dd993c4df1fbe
abmehmet <mehmetercan5755@gmail.com>
2022-09-30 16:06:48 +0300

    first commit v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("RentaBook !");
+        System.out.println("This is a Conseole Application for RentaBook Project !");
     }
 }
